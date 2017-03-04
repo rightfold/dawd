181b06d1ca1a9348a38b33a1e5d2fa4a9a023451
rightfold <rightfold@gmail.com>
2017-03-04 18:20:27 +0100

    Render HTML in Mustache template

--- server/src/dawdd.js
+++ server/src/dawdd.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var document_html = require('./document/html');
+var fs = require('fs');
 var $module = require('./module');
+var mustache = require('mustache');
 var path = require('path');
 
 var levelPaths = {};
@@ -8,21 +10,41 @@ levelPaths[$module.Level.System]       = 'system';
 levelPaths[$module.Level.Application]  = 'application';
 levelPaths[$module.Level.Installation] = 'installation';
 
+function render(layout, dawdModule, callback) {
+  var document = new document_html.HTMLDocument();
+  dawdModule.render(document, function(err) {
+    if (err !== null) {
+      callback(err, null);
+      return;
+    }
+    var html;
+    try {
+      html = mustache.render(layout, {
+        body: document.html,
+      });
+    } catch (e) {
+      callback(e, null);
+      return;
+    }
+    callback(null, html);
+  });
+}
+
 function main() {
+  var layout = fs.readFileSync(process.argv[2], 'utf8');
   var app = express();
-  process.argv.slice(2).forEach(function(dawdModulePath) {
+  process.argv.slice(3).forEach(function(dawdModulePath) {
     var dawdModule = require(path.resolve(dawdModulePath))($module);
     var levelPath = levelPaths[dawdModule.level];
     app.get('/report/' + levelPath + '/' + dawdModule.name, function(req, res) {
-      var document = new document_html.HTMLDocument();
-      dawdModule.render(document, function(err) {
+      render(layout, dawdModule, function(err, html) {
         if (err !== null) {
           res.status(500);
           res.end();
           return;
         }
         res.contentType('text/html');
-        res.end(document.html);
+        res.end(html);
       });
     });
   });
