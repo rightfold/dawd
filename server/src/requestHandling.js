84f538bc78bf16d4e7cbd39d3220f011d0f7dcd7
rightfold <rightfold@gmail.com>
2017-03-06 18:11:10 +0100

    Add plain text documents

--- server/src/requestHandling.js
+++ server/src/requestHandling.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var document_html = require('./document/html');
+var document_text = require('./document/text');
 var $module = require('./module');
 var mustache = require('mustache');
 
@@ -24,7 +25,7 @@ function requestArguments(dawdModule, req) {
   throw Error('Unknown module class: ' + dawdModule.constructor.name);
 }
 
-function render(dbPool, layout, dawdModule, $arguments, callback) {
+function renderHTML(dbPool, layout, dawdModule, $arguments, callback) {
   var document = new document_html.HTMLDocument();
   dawdModule.render(dbPool, $arguments, document, function(err) {
     if (err !== null) {
@@ -45,22 +46,42 @@ function render(dbPool, layout, dawdModule, $arguments, callback) {
   });
 }
 
+function renderText(dbPool, layout, dawdModule, $arguments, callback) {
+  var document = new document_text.TextDocument();
+  dawdModule.render(dbPool, $arguments, document, function(err) {
+    if (err !== null) {
+      callback(err, null);
+      return;
+    }
+    callback(null, document.text);
+  });
+}
+
 function installModuleHandlers(dbPool, layout, app, dawdModule) {
+  var method = requestMethod(dawdModule);
+
   var moduleTypePath = $module.moduleTypePathSegment(dawdModule);
   var levelPath = $module.levelPathSegment(dawdModule.level);
-  var method = requestMethod(dawdModule);
-  app[method]('/' + moduleTypePath + '/' + levelPath + '/' + dawdModule.name, function(req, res) {
-    var $arguments = requestArguments(dawdModule, req);
-    render(dbPool, layout, dawdModule, $arguments, function(err, html) {
-      if (err !== null) {
-        res.status(500);
-        res.end();
-        return;
-      }
-      res.contentType('text/html');
-      res.end(html);
+  var basePath = '/' + moduleTypePath + '/' + levelPath + '/' + dawdModule.name;
+
+  register(basePath, 'text/html', renderHTML);
+  register(basePath + '.html', 'text/html', renderHTML);
+  register(basePath + '.txt', 'text/plain', renderText);
+
+  function register(path, contentType, render) {
+    app[method](path, function(req, res) {
+      var $arguments = requestArguments(dawdModule, req);
+      render(dbPool, layout, dawdModule, $arguments, function(err, body) {
+        if (err !== null) {
+          res.status(500);
+          res.end();
+          return;
+        }
+        res.contentType(contentType);
+        res.end(body);
+      });
     });
-  });
+  }
 }
 
 module.exports = {
