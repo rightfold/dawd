53bdc44fda8bce5bf0c0e4a8658f382649d940c7
rightfold <rightfold@gmail.com>
2017-03-11 13:31:27 +0100

    Throw on all errors

--- server/index.php
+++ server/index.php
@@ -1,3 +1,10 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+error_reporting(E_ALL);
+set_error_handler(function($severity, $message, $file, $line) {
+    if (!(error_reporting() & $severity)) {
+        return;
+    }
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
 DAWD\Main::main();
